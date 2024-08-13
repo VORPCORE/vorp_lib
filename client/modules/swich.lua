2ffa35ed39959e3a83ba544674435dce29c5accb
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-08-13 15:51:06 +0100

    feat(client): add state to stop `SetInterval` callbacks

--- client/modules/swich.lua
+++ client/modules/swich.lua
@@ -38,29 +38,11 @@ local function switch(value)
     return Switch:new(value)
 end
 
----EXAMPLE
---local result = Switch(1) -- parameter is the value to be checked
---    :case("hello", function()
---        return "Hello, World!"
---    end)
---    :case(1, function()
---        return "World, Hello!"
---    end)
---    :case(true, function()
---        return "World, Hello!"
---    end)
---    :default(function()
---        return "Hello, Hello!"
---    end)
---    :execute()
---
---print("Result:", result)
---
-
 ---@class Interval
 ---@field callback fun():any
 ---@field delay integer
 ---@field id string
+---@field state boolean
 ---@field execute fun(self:Interval):any
 local Interval = {}
 Interval.__index = Interval
@@ -70,53 +52,35 @@ end
 
 ---@constructor
 function Interval:new(callback, delay, id)
-    local properties = { callback = callback, delay = delay or 1000, id = id }
+    local properties = { callback = callback, delay = delay or 1000, id = id, state = true }
     return setmetatable(properties, self)
 end
 
 ---@methods
 function Interval:execute()
-    Citizen.CreateThreadNow(function()
-        while true do
+    Citizen.CreateThread(function()
+        while self.state do
             self.callback()
             Wait(self.delay)
         end
     end)
 end
 
+function Interval:Destroy()
+    self.callback = nil
+    self.delay = nil
+    self.id = nil
+    self.state = nil
+end
+
 ---@initializer
-function SetInterval(callback, delay, id)
+local function setInterval(callback, delay, id)
     local interval = Interval:new(callback, delay, id)
     interval:execute()
     return interval
 end
 
--- example
---local instance = SetInterval(function()
---    print("Hello, World!")
---end, 1000, "hello")
-
----@param t table the table to iterate either array or dictionary
----@param func fun(k:any, v:any)
----@param array boolean? use ipairs or pairs
-local function forEach(t, func, array)
-    if not t and type(t) ~= "table" then return end
-
-    if array then
-        for k, v in ipairs(t) do func(k, v) end
-    else
-        for k, v in pairs(t) do func(k, v) end
-    end
-end
-
----EXAMPLE
---local t = { 1, 2, 3, 4, 5 }
---forEach(t, function(k, v) -- built in nil check and type check
---    print(k, v)
---end, true)
-
 return {
     Switch = switch,
-    Foreach = forEach,
-    SetInterval = SetInterval
+    SetInterval = setInterval
 }
