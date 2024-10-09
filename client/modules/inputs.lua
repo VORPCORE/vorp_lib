88056817fd6e1b10f49c80356d6ea6596333de30
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-10-09 23:24:06 +0100

    feat(client/inputs): allow string or hash keys + custom args in callbacks

--- client/modules/inputs.lua
+++ client/modules/inputs.lua
@@ -1,7 +1,9 @@
 local LIB <const> = Import "classes"
 
+
 ---@class Inputs
 ---@field private New fun(self: Inputs, key: string | number, callback: function, type: string, state: boolean): Inputs
+---@field private NormalizeKey fun(self: Inputs, key: string | number): string | number
 ---@field public IsRunning function check if the instance is running
 ---@field public Start function start look up for input
 ---@field public Remove function destroy the instance
@@ -12,7 +14,7 @@ local LIB <const> = Import "classes"
 local Inputs = {}
 
 ---@static
-local inputTypes = {
+local inputTypes <const> = {
     Press = IsControlJustPressed,
     Hold = IsControlPressed,
     Release = IsControlJustReleased
@@ -20,25 +22,23 @@ local inputTypes = {
 
 --todo: import input keys data instead of having them here
 ---@static
-local inputKeys = {
+local inputKeys <const> = {
     G = `INPUT_INTERACT_ANIMAL`,
 }
 
-local input = LIB.Class.Create({
-
+local input = LIB.Class:Create({
     constructor = function(self, data)
         self.key = data.key
         self.callback = data.callback
         self.inputType = data.inputType
         self.isRunning = data.isRunning
+        self.customParams = {}
     end,
-
     get = {
         IsRunning = function(self)
             return self.isRunning
         end,
     },
-
     set = {
         Remove = function(self)
             self.isRunning = nil
@@ -50,10 +50,14 @@ local input = LIB.Class.Create({
             if not self.isRunning then return end
             self.isRunning = false
         end,
-        Resume = function(self)
+        Resume = function(self, ...)
+            self:Update(...)
             if self.isRunning then return end
-            self.isRunning = true
+            self.isRunning = false
             self:Start()
+        end,
+        Update = function(self, ...)
+            self.customParams = { ... }
         end,
         Start = function(self)
             if self.isRunning then return end
@@ -62,7 +66,7 @@ local input = LIB.Class.Create({
                 while self.isRunning do
                     Wait(0)
                     if self.inputType(0, self.key) then
-                        self.callback(self)
+                        self.callback(self, table.unpack(self.customParams))
                     end
                 end
             end)
@@ -70,22 +74,33 @@ local input = LIB.Class.Create({
     }
 })
 
+function Inputs:NormalizeKey(key)
+    if type(key) == 'string' then
+        local sub = string.sub(key, 1, 1)
+        if sub then
+            if not inputKeys[key] then
+                error(('input key %s does not exist, available keys are %s'):format(key, table.concat(inputKeys, ', ')))
+            end
+            key = inputKeys[key]
+        end
+    end
+
+    return key
+end
 
 function Inputs:Register(key, callback, inputType, state)
     if not inputTypes[inputType] then
         error(('input type %s does not exist, available types are %s'):format(inputType, table.concat(inputTypes, ', ')))
     end
 
-    if type(key) == 'string' then
-        if not inputKeys[key] then
-            error(('input key %s does not exist, available keys are %s'):format(key, table.concat(inputKeys, ', ')))
-        end
-        key = inputKeys[key]
-    end
+    key = self:NormalizeKey(key)
+
     local instance = input:new({ key = key, callback = callback, inputType = inputTypes[inputType] })
+
     if state then
         instance:Start()
     end
+
     return instance
 end
 
