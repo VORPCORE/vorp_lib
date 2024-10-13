d2e63ea79cf439da5fca1e2ec1320fb5f97798a4
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-10-13 18:19:46 +0100

    refactor(client/functions): use lib class for interval and switch + allow custom args for SetInterval callbacks

--- client/modules/functions.lua
+++ client/modules/functions.lua
@@ -1,81 +1,91 @@
+local LIB = Import 'classes'
+
 ---@class Switch
 ---@field cases table
----@field default fun(self:Switch, value:any):any
 ---@field value any
----@field execute fun(self:Switch):any
+---@field public default fun(self:Switch, value:any):any
+---@field public execute fun(self:Switch):any
 local Switch = {}
-Switch.__index = Switch
-Switch.__call = function()
-    return "Switch"
-end
 
----@constructor
-function Switch:new(value)
-    local properties = { cases = {}, default = nil, value = value }
-    return setmetatable(properties, self)
-end
-
----@methods
-function Switch:case(value, func)
-    self.cases[value] = func
-    return self
-end
-
-function Switch:default(func)
-    self.default = func
-    return self
-end
-
-function Switch:execute()
-    local case_func = self.cases[self.value] or self.default
-    if case_func then
-        return case_func(self.value)
+local switchs = LIB.Class:Create({
+    constructor = function(self, value)
+        self.cases = {}
+        self.default = nil
+        self.value = value
+    end,
+    case = function(self, value, func)
+        self.cases[value] = func
+        return self
+    end,
+    default = function(self, func)
+        self.default = func
+        return self
+    end,
+    execute = function(self)
+        local case_func = self.cases[self.value] or self.default
+        if case_func then
+            return case_func(self.value)
+        end
     end
-end
+})
 
----@initializer
-local function switch(value)
-    return Switch:new(value)
-end
 
 ---@class Interval
----@field callback fun():any
+---@field callback fun(self:Interval):any
 ---@field delay integer
 ---@field id string
 ---@field state boolean
----@field execute fun(self:Interval):any
+---@field customArgs table
+---@field private execute fun(self:Interval):any
+---@field public Pause fun(self:Interval)
+---@field public Resume fun(self:Interval, ...:any)
+---@field public Destroy fun(self:Interval)
+---@field public Update fun(self:Interval, ...:any)
 local Interval = {}
-Interval.__index = Interval
-Interval.__call = function()
-    return "Interval"
-end
 
----@constructor
-function Interval:new(callback, delay, id)
-    local properties = { callback = callback, delay = delay or 1000, state = true }
-    return setmetatable(properties, self)
-end
+local intervals = LIB.Class:Create({
+    constructor = function(self, callback, delay, customArgs)
+        self.callback = callback
+        self.delay = delay or 1000
+        self.state = true
+        self.customArgs = customArgs or {}
+    end,
+    execute = function(self)
+        CreateThread(function()
+            while self.state do
+                self.callback(self, table.unpack(self.customArgs))
+                Wait(self.delay)
+            end
+        end)
+    end,
+    Destroy = function(self)
+        self.callback = nil
+        self.delay = nil
+        self.state = nil
+        self.customArgs = nil
+    end,
+    Pause = function(self)
+        if not self.state then return print("interval is not running") end
+        self.state = false
+    end,
+    Resume = function(self, ...)
+        if self.state then return print("interval is already running") end
+        self.state = true
+        self.customArgs = { ... }
+        self:execute()
+    end,
+    Update = function(self, ...)
+        self.customArgs = { ... }
+    end
 
----@methods
-function Interval:execute()
-    Citizen.CreateThread(function()
-        while self.state do
-            self.callback()
-            Wait(self.delay)
-        end
-    end)
-end
+})
 
-function Interval:Destroy()
-    self.callback = nil
-    self.delay = nil
-    self.state = nil
+---@initializers
+local function switch(value)
+    return switchs:new(value)
 end
-
----@initializer
-local function setInterval(callback, delay)
-    local interval = Interval:new(callback, delay)
-    interval:execute()
+local function setInterval(callback, delay, customArgs)
+    local interval = intervals:new(callback, delay, customArgs)
     return interval
 end
 
