f9c3e8a13665d278f23b51d006a0eb588edf893a
Outsider <87246847+outsider31000@users.noreply.github.com>
2025-04-07 04:27:33 +0100

    feat(shared): add CreateNewThread function
    moved functions file to shared module so it can be used by both server and client
    
    implemented CreateNewThread
    that returns methods like start stop

---
+++ shared/function.lua
@@ -0,0 +1,188 @@
+local LIB = Import 'class'
+
+---@class Switch
+---@field cases table
+---@field value any
+---@field public default fun(self:Switch, value:any):any
+---@field public execute fun(self:Switch):any
+local Switch = {}
+
+local switchs = LIB.Class:Create({
+    constructor = function(self, value)
+        self.cases = {}
+        self.default = nil
+        self.value = value
+    end,
+    case = function(self, value, func)
+        self.cases[value] = func
+        return self
+    end,
+    default = function(self, func)
+        self.default = func
+        return self
+    end,
+    execute = function(self)
+        local case_func = self.cases[self.value] or self.default
+        if case_func then
+            return case_func(self.value)
+        end
+    end
+})
+
+
+---@class Interval
+---@field callback fun(self:Interval):any
+---@field delay integer
+---@field id string
+---@field state boolean
+---@field customArgs table
+---@field private execute fun(self:Interval):any
+---@field public Pause fun(self:Interval)
+---@field public Resume fun(self:Interval, ...:any)
+---@field public Destroy fun(self:Interval)
+---@field public Update fun(self:Interval, ...:any)
+local Interval = {}
+
+local intervals = LIB.Class:Create({
+    constructor = function(self, callback, delay, customArgs)
+        self.callback = callback
+        self.delay = delay or 1000
+        self.state = true
+        self.customArgs = customArgs or {}
+    end,
+    execute = function(self)
+        CreateThread(function()
+            while self.state do
+                self.callback(self, table.unpack(self.customArgs))
+                Wait(self.delay)
+            end
+        end)
+    end,
+    Destroy = function(self)
+        self.callback = nil
+        self.delay = nil
+        self.state = nil
+        self.customArgs = nil
+    end,
+    Pause = function(self)
+        if not self.state then return print("interval is not running") end
+        self.state = false
+    end,
+    Resume = function(self, ...)
+        if self.state then return print("interval is already running") end
+        self.state = true
+        self.customArgs = { ... }
+        self:execute()
+    end,
+    Update = function(self, ...)
+        self.customArgs = { ... }
+    end,
+    GetState = function(self)
+        return self.state
+    end
+
+})
+
+--create a setTimeout function same as interval return ids so we can close it
+local timeouts = LIB.Class:Create({
+    constructor = function(self, callback, delay, customArgs)
+        self.callback = callback
+        self.delay = delay
+        self.customArgs = customArgs
+        self.state = true
+    end,
+    execute = function(self)
+        if not self.state then return end
+        CreateThread(function()
+            Wait(self.delay)
+            if not self.state then return end
+            self.callback(table.unpack(self.customArgs))
+        end)
+    end,
+    Destroy = function(self)
+        self.state = nil
+        self.callback = nil
+        self.delay = nil
+        self.customArgs = nil
+    end,
+
+    Pause = function(self)
+        if not self.state then return print("timeout is not running to be paused") end
+        self.state = false
+    end,
+
+    Resume = function(self, ...)
+        if self.state then return print("timeout is already running to be resumed") end
+        self.state = true
+        self.customArgs = { ... }
+        self:execute()
+    end,
+
+    Update = function(self, ...)
+        self.customArgs = { ... }
+    end,
+    GetState = function(self)
+        return self.state
+    end
+
+})
+
+-- Define the Thread class using a metatable
+
+local threads = LIB.Class:Create({
+
+    constructor = function(self, func, start)
+        self.coroutine = coroutine.create(func)
+        self.state = false
+        if start then
+            self:execute()
+        end
+    end,
+
+    execute = function(self)
+        if self.state then return print("thread is already running") end
+        self.state = true
+        coroutine.resume(self.coroutine)
+    end,
+
+    Pause = function(self)
+        if not self.state then return print("thread is not running to be paused") end
+        self.state = false
+    end,
+
+    Destroy = function(self)
+        self.state = nil
+        self.coroutine = nil
+    end,
+
+    Resume = function(self)
+        if self.state then return print("thread is already running") end
+        self:execute()
+    end,
+
+    GetState = function(self)
+        return self.state
+    end
+
+})
+
+---@initializers
+local function switch(value)
+    return switchs:new(value)
+end
+local function setInterval(callback, delay, customArgs)
+    return intervals:new(callback, delay, customArgs)
+end
+local function setTimeout(callback, delay, customArgs)
+    return timeouts:new(callback, delay, customArgs)
+end
+local function createNewThread(func, start)
+    return threads:new(func, start)
+end
+
+return {
+    Switch = switch,
+    SetInterval = setInterval,
+    SetTimeout = setTimeout,
+    CreateNewThread = createNewThread
+}
