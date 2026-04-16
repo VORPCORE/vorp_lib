6aa71b3e668c097b29440decef5b1a8a9e8a0629
lu <contact@lm0uta.com>
2026-04-16 18:39:58 +0200

    feat(client): remove unnecessary NUI focus and wait in copyToClipBoard function

--- client/main/copy.lua
+++ client/main/copy.lua
@@ -2,12 +2,10 @@ exports('copyToClipBoard', function(text)
     if not text or type(text) ~= "string" then
         return
     end
-    SetNuiFocus(true, false)
     SendNUIMessage({
         data = {
             type = 'copy',
             text = text
         }
     })
-    Wait(500)
 end)
